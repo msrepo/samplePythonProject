415a43f6410358fa98368028c1bdc96134a970d1
msrepo <maheshshakya.01@gmail.com>
2021-10-06 06:36:52 +0545

    added deriv

--- src/type_checked_funcs.py
+++ src/type_checked_funcs.py
@@ -1,5 +1,8 @@
 import numpy as np
 from numpy import ndarray
+from typing import Callable
+
+from numpy.lib.function_base import delete
 
 
 def square(array: ndarray) -> ndarray:
@@ -13,3 +16,21 @@ def square(array: ndarray) -> ndarray:
     :rtype: ndarray
     """
     return array * array
+
+
+def derivative(func: Callable, x: float, delta: float) -> float:
+    """calculate derivative of func at x
+
+    numerically calculate derivative of func at x by calculating rate of change in the neighbourhood
+    delta of x 
+
+    :param func: any Callable
+    :type func: Callable
+    :param x: calculat func at
+    :type x: float
+    :param delta: neighborhood 
+    :type delta: float
+    :return: derivative of func at x in neighborhood delta
+    :rtype: float
+    """
+    return (func(x + delta) - func(x - delta)) / (2.0 * delta)
--- test/test01.py
+++ test/test01.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 from context import src
-from src.type_checked_funcs import square
+from src.type_checked_funcs import square, derivative
 
 
 class FuncTests(unittest.TestCase):
@@ -14,6 +14,15 @@ class FuncTests(unittest.TestCase):
         a = np.array([1, 2, 3, 4])
         b = np.array([1, 4, 9, 16])
         assert((square(a) == b).all())
+
+    def test_derivative(self):
+        def func(x):
+            return x * x
+        a = 2.0
+        da = 4.0
+        delta = 1e-4
+        out = derivative(func, a, delta)
+        assert(da - out < 1e-5)
 
 
 if __name__ == '__main__':
